6811b55f6f9b0e3cb171bb117df7a25a51f575d0
kevinykuo <kevinykuo@gmail.com>
2019-12-10 23:59:29 -0800

    remove read/rename test

--- tests/testthat/test-workflow.R
+++ tests/testthat/test-workflow.R
@@ -13,31 +13,3 @@ test_that("unzip works", {
 
   expect_identical(ls_1, ls_2)
 })
-
-test_that("read and rename", {
-  col_specs <- gisa_col_specs()
-  headers <- gisa_headers()
-
-  data_liab <- gisa_data_dir() %>%
-    path("CLSP") %>%
-    dir_ls() %>%
-    map_dfr(read_csv,
-      col_names = headers$liability,
-      col_types = col_specs$liability
-    ) %>%
-    gisa_rename_cols()
-
-  expect_identical(nrow(data_liab), 15L)
-  expect_identical(
-    names(data_liab),
-    c(
-      "written_premium", "earned_premium", "reported_claim_count",
-      "generated_claim_count", "loss_amount", "expense_amount", "loss_and_expense_amount",
-      "section_number", "company_identification", "region", "province",
-      "valuation_year", "accident_year", "entry_year", "major_class",
-      "coverage_policy_form", "industry_code", "policy_type", "kind_of_loss_code",
-      "kind_of_loss_indicator", "paid_outstanding_indicator", "claim_location",
-      "type_of_expense", "policy_limit_group", "size_of_loss_range_group"
-    )
-  )
-})
