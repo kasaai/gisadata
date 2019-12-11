1726f4826d6a0d7a04dc4e319d7b391f36bbb10a
kevinykuo <kevinykuo@gmail.com>
2019-12-10 16:56:52 -0800

    column selector function

--- R/col-mapping.R
+++ R/col-mapping.R
@@ -17,3 +17,65 @@ gisa_rename_cols <- function(data) {
     data
   }
 }
+
+#' Select columns based on format number
+#'
+#' @param data Data frame
+#' @param format_number GISA format number
+#' @export
+gisa_select_cols <- function(data, format_number) {
+  cols_to_select <- switch(
+    format_number,
+    "3" = c(
+      "generated_claim_amount",
+      "loss_amount",
+      "expense_amount",
+      "loss_and_expense_amount",
+      "section_number",
+      "company_identification",
+      "region",
+      "province",
+      "valuation_year",
+      "accident_year",
+      "major_class",
+      "coverage_policy_form",
+      "kind_of_loss_indicaator",
+      "size_of_loss_range"
+    ),
+    "4" = c(
+      "generated_claim_amount",
+      "loss_amount",
+      "expense_amount",
+      "loss_and_expense_amount",
+      "section_number",
+      "company_identification",
+      "region",
+      "province",
+      "valuation_year",
+      "accident_year",
+      "industry_code",
+      "size_of_loss_range"
+    ),
+    "10" = c(
+      "generated_claim_amount",
+      "loss_amount",
+      "expense_amount",
+      "loss_and_expense_amount",
+      "section_number",
+      "company_identification",
+      "region",
+      "province",
+      "valuation_year",
+      "accident_year",
+      "entry_year",
+      "coverage_policy_form",
+      "policy_type",
+      "kind_of_loss_code",
+      "kind_of_loss_indicator",
+      "paid_outstanding_indicator"
+    )
+  )
+
+  data %>%
+    dplyr::select(!!cols_to_select)
+}
