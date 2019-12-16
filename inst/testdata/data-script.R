3feae99115b0b742bee1e7d8af9c8c49bd55a4c0
kevinykuo <kevinykuo@gmail.com>
2019-12-16 07:15:56 -0800

    fix data generation script

--- inst/testdata/data-script.R
+++ inst/testdata/data-script.R
@@ -10,10 +10,27 @@ c("Auto Cat Report", "Auto Loss Development", "CLSP") %>%
     folder_path %>%
       dir_ls() %>%
       walk(function(file_path) {
-        file_path %>%
-          read_csv(col_names = FALSE) %>%
-          mutate_at(vars(-one_of(c("X13", "X46"))), ~ sample(.x, length(.x))) %>%
-          group_by(X13, X46) %>%
+        df <- file_path %>%
+          read_csv(col_names = FALSE)
+
+        if (folder == "Auto Loss Development") {
+          df <- df %>%
+            # Don't permute: SectionNumber, Province, EntryHalfYear, AccidentHalfYear
+            mutate_at(vars(-one_of(c("X13", "X46", "X57", "X58"))), ~ sample(.x, length(.x))) %>%
+            group_by(X13, X46)
+        } else if (folder == "CLSP") {
+          df <- df %>%
+            # Don't permute: Section Number, Accident Year, Entry Year
+            mutate_at(vars(-one_of(c("X13", "X18", "X19"))), ~ sample(.x, length(.x))) %>%
+            group_by(X13)
+        } else {
+          df <- df %>%
+            # Don't permute: SectionNumber, Province
+            mutate_at(vars(-one_of(c("X13", "X46", "X57", "X58"))), ~ sample(.x, length(.x))) %>%
+            group_by(X13, X46)
+        }
+
+        df %>%
           sample_n(min(n(), 100)) %>%
           ungroup() %>%
           write_csv(file_path, col_names = FALSE, na = "")
