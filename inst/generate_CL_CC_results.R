7edbb8b3bd55790fc74130633ec8d9ddb6087efe
Marco De Virgilis <devirgilis.marco@gmail.com>
2021-03-17 17:19:30 -0500

    added readme for results

--- inst/generate_CL_CC_results.R
+++ inst/generate_CL_CC_results.R
@@ -1,85 +1,90 @@
 library(readxl)
 library(ChainLadder)
 library(openxlsx)
-options(scipen = 999)
-
+library(tidyverse)
 
 print_cl_results <- function(filename){
-  
+
   cl_results <- function(amount){
-    
-    ### this function is fed either with the dataframe of paid or incurred from the excel files in the folder
-    
+
     amount %>% select(accident_half_year) %>% bind_cols(
       (amount %>% select(-accident_half_year) %>%
-         as.matrix %>% as.triangle() %>% 
+         as.matrix %>% as.triangle() %>%
          MackChainLadder() %>% summary)[[1]][,c(1,3,4)]
     ) %>% return()
-    
-    
+
+
   }
-  
+
   amounts_vector <- c("paid_amounts","incurred_amounts")
-  
-  amounts_vector %>% map(~read_excel(filename, 
+
+  amounts_vector %>% map(~read_excel(filename,
                                      sheet = .)
-  ) %>% map(~cl_results(.)) %>% set_names(amounts_vector) %>% 
+  ) %>% map(~cl_results(.)) %>% set_names(amounts_vector) %>%
     write.xlsx(paste0(str_remove(filename,("\\.xlsx$")),"_Chain_Ladder.xlsx"))
-  
+
 }
-
-list.files()[3] %>% print_cl_results()
-
 
 print_cape_cod_results <- function(filename){
 
   cape_cod <- function(amount,exposure){
-    
+
     pattern <- (amount %>% select(-accident_half_year) %>%
                   as.matrix %>% as.triangle() %>% MackChainLadder() %>% summary())[[1]][,2]
-    
+
     uup <- (pattern * exposure) %>% sum
-    
+
     latest <- (amount %>% select(-accident_half_year) %>%
                  as.matrix %>% as.triangle() %>% MackChainLadder() %>% summary())[[1]][,1]
-    
+
     lr_implied <- (latest %>% sum) / uup
-    
+
     cape_cod_ultimate <- latest + (1 - pattern) * lr_implied * exposure
-    
+
     tibble(
-      
       amount[,1],
       Latest = latest,
       Ultimate = cape_cod_ultimate,
       IBNR = Ultimate - Latest
     ) %>% return()
-    
-    
+
+
   }
-    
+
   amounts_vector <- c("paid_amounts","incurred_amounts")
-  
-  amounts_list <- amounts_vector %>% map(~read_excel(filename, 
-                                                     sheet = .)
-                                         )
-  
+
+  amounts_list <- amounts_vector %>% map(~read_excel(filename, sheet = .))
+
   premium_list <- read_excel(filename, sheet = "premiums_exposures")
-  
-  amounts_list %>% map(~cape_cod(.,premium_list %>% pull(premium_earned))) %>% set_names(amounts_vector) %>% 
-  write.xlsx(paste0(str_remove(filename,("\\.xlsx$")),"_Cape_cod.xlsx"))
-  
-  
+
+  amounts_list %>% map(~cape_cod(.,premium_list %>% pull(premium_earned))) %>% set_names(amounts_vector) %>%
+  write.xlsx(paste0(str_remove(filename,("\\.xlsx$")),"_Cape_Cod.xlsx"))
+
+
 }
 
-print_cape_cod_results(list.files()[3])
 
 
+print_cl_results("Alberta_Collision.xlsx")
+print_cl_results("Alberta_TPL_BI.xlsx")
+print_cl_results("Atlantic_Canada_Collision.xlsx")
+print_cl_results("Atlantic_Canada_TPL_BI.xlsx")
+print_cl_results("Nova_Scotia_Collision.xlsx")
+print_cl_results("Nova_Scotia_TPL_BI.xlsx")
+print_cl_results("Ontario_AB_DI.xlsx")
+print_cl_results("Ontario_AB_MR.xlsx")
+print_cl_results("Ontario_Collision.xlsx")
+print_cl_results("Ontario_TPL_BI.xlsx")
 
-
-
-
-
-
+print_cape_cod_results("Alberta_Collision.xlsx")
+print_cape_cod_results("Alberta_TPL_BI.xlsx")
+print_cape_cod_results("Atlantic_Canada_Collision.xlsx")
+print_cape_cod_results("Atlantic_Canada_TPL_BI.xlsx")
+print_cape_cod_results("Nova_Scotia_Collision.xlsx")
+print_cape_cod_results("Nova_Scotia_TPL_BI.xlsx")
+print_cape_cod_results("Ontario_AB_DI.xlsx")
+print_cape_cod_results("Ontario_AB_MR.xlsx")
+print_cape_cod_results("Ontario_Collision.xlsx")
+print_cape_cod_results("Ontario_TPL_BI.xlsx")
 
 
